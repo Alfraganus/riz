c507cb90b234d9c22a9770e7e95ff50b306a3817
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2024-09-09 04:16:11 +0500

    initial commint

--- app/Modules/openAi/routes.php
+++ app/Modules/openAi/routes.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/upload-chat-screenshot', [\App\Modules\openAi\controllers\OpenAIController::class, 'processScreenshot']);
-Route::post('/recognize-object', [OpenAiController::class, 'recognizeObject']);
+Route::post('/get-gpt-advice', [OpenAiController::class, 'gptAdvice']);

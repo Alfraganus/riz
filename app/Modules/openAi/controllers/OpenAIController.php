6bba1d0a6b2936b68c6f939141326ebefcb6e8bf
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2024-09-10 22:00:34 +0500

    initial commint

--- app/Modules/openAi/controllers/OpenAIController.php
+++ app/Modules/openAi/controllers/OpenAIController.php
@@ -3,6 +3,7 @@
 namespace App\Modules\openAi\controllers;
 
 use App\Modules\openAi\service\OpenAiImageChatService;
+use App\Modules\openAi\service\OpenAiRandomPickService;
 use App\Modules\openAi\service\OpenAiTextChatService;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
@@ -11,12 +12,14 @@ class OpenAIController extends BaseController
 {
 
     public function __construct(
-        OpenAiImageChatService $openAiService,
-        OpenAiTextChatService  $aiTextChatService
+        OpenAiImageChatService   $openAiService,
+        OpenAiTextChatService    $aiTextChatService,
+        OpenAiRandomPickService  $openAiRandomPickService
     )
     {
         $this->openAiService = $openAiService;
         $this->aiTextChatService = $aiTextChatService;
+        $this->openAiRandomPickService = $openAiRandomPickService;
     }
 
     public function gptAdviceFromImage(Request $request)
@@ -27,5 +30,10 @@ class OpenAIController extends BaseController
     public function gptAdviceFromText(Request $request)
     {
         return $this->aiTextChatService->sendTextChatToGPT($request);
+    }
+
+    public function gptRandomPick(Request $request)
+    {
+        return $this->openAiRandomPickService->sendRandomPickGPT($request);
     }
 }

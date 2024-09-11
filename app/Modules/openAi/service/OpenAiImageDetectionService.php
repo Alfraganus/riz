b2c9783abc44420ef22b3e45075a53bc3b2f96e8
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2024-09-11 14:26:03 +0500

    123

--- app/Modules/openAi/service/OpenAiImageDetectionService.php
+++ app/Modules/openAi/service/OpenAiImageDetectionService.php
@@ -75,6 +75,9 @@ class OpenAiImageDetectionService extends OpenAiService
 
         $cleanJsonString = str_replace(['```json', '```'], '', $results);
 
-        return $cleanJsonString;
+        $payload = json_decode($cleanJsonString,true);
+//        unset($payload);
+        return $payload;
     }
+
 }
